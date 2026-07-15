66fd8a14f12c57655f03b0948c137bb8c789cb5e
Jacob HL <jacobhl3ca@gmail.com>
2026-07-14 22:27:02 -0400

    Sentry cleanup: remove root dup + filter browser-extension noise (#32)
    * cleanup: remove redundant root instrumentation-client.ts (src/ one is the active file)
    
    * chore(sentry): filter browser-extension noise (EmptyRanges, sendMessage, svg image)

--- instrumentation-client.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: "https://b1d8b3efe70b8dbe7f865cd0b5dc832a@o4511667913818112.ingest.us.sentry.io/4511694546534400",
-  tracesSampleRate: 0.1,
-  enableLogs: true,
-});
-
-export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
